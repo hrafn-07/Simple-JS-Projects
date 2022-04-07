c204e6ab926c1ee93adcd2ce045c0475ff754ffd
hrafn-13 <kuzgun.develop@protonmail.com>
2022-04-07 02:37:04 +0000

    Simple JS Projects

--- Item-Filter/js/app.js
+++ Item-Filter/js/app.js
@@ -1,46 +1,46 @@
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("card");
-  if (c == "all") c = "All";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-// Show filtered elements
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
+let button = document.querySelectorAll('.btn');
+let items = document.querySelectorAll('.card');
+
+
+ button.forEach((btn) => {
+  btn.addEventListener('click', event => {
+    //Activate Filter Button
+    for (let i = 0; i < button.length; i++) {
+      button[i].classList.remove('active');
     }
-  }
-  
-  // Hide elements that are not selected
-  function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-  
-  // Add active class to the current control button (highlight it)
-  var btnContainer = document.getElementsByClassName("nav");
-  var btns = btnContainer.getElementsByClassName("btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
+    event.target.classList.add('active');
+
+    //filter items with brand
+    items.forEach((item) => {
+      let brand = item.querySelector('.card-sub');  //brand is a class under the item
+      if(event.target.innerText == 'All')           //clicked items inner text
+        {
+          item.style.display = 'block';
+        }
+        else{
+          if(brand.innerText == event.target.innerText)
+          {
+            item.style.display = 'block'
+          }else{
+            item.style.display = 'none'
+          }
+        }
+
     });
-  }
+  });
+});
+const searchBox = document.querySelector('#search-filter');
+
+searchBox.addEventListener('keyup', (e) => {
+    const searchFilter = e.target.value.toLowerCase().trim();
+    
+    //display only items that contain filter input
+    items.forEach((item) => {
+      let srchLower = item.innerText.toLowerCase();
+        if (srchLower.includes(searchFilter)){
+            item.style.display = 'block'
+         } else {
+             item.style.display = 'none'
+         }
+    })
+ });
